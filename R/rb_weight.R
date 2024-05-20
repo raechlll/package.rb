126c38d48ed67ddffcdba3d2a0bfb467e50d2d7c
Rachel <rachel.breuer@student.hu.nl>
2024-05-20 23:59:54 +0200

    add

---
+++ R/rb_weight.R
@@ -0,0 +1,27 @@
+#' Omzetten van gewichtsmetingen (kg, g en mg)
+#'
+#' @param value waarde die omgezet moet worden
+#' @param from eenheid waarvan de waarde afkomstig is
+#' @param to eenheid waarvan de waarde omgezet moet worden
+#'
+#' @return waarde met omgezette eenheid
+#' @export
+#'
+#' @examples
+rb_weight <- function(value, from, to) {
+       if (from == "kg" && to == "g") {
+             return(value * 1000)
+         } else if (from == "g" && to == "kg") {
+               return(value / 1000)
+           } else if (from == "g" && to == "mg") {
+                 return(value * 1000)
+             } else if (from == "mg" && to == "g") {
+                   return(value / 1000)
+               } else if (from == "kg" && to == "mg") {
+                     return(value * 1000000)
+                 } else if (from == "mg" && to == "kg") {
+                       return(value / 1000000)
+                   } else {
+                         return("Ongeldige eenheid. Gebruik 'kg', 'g' of 'mg'.")
+                     }[[1]]
+   }
